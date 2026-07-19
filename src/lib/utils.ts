acd3913761dbdc086378c5ceaeb0f161014cc581
Ahmed Ibrahim <ahmed.ibrahim.elsayed01@gmail.com>
2026-07-19 21:18:23 +0300

    feat: add breakpoints

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export const BREAKPOINTS = {
+  sm: 640,
+  md: 768,
+  lg: 1024,
+  xl: 1280,
+};
